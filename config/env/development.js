2ede8a1135453457723ca3d8e2c45cebbb829e55
Thomas Sarboni <max-k@post.com>
2015-03-08 23:11:06 +0100

    Update docker configuration to use compose

--- config/env/development.js
+++ config/env/development.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-	db: 'mongodb://localhost/javascript-fullstack-store-dev',
+	db: 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/javascript-fullstack-store-dev',
 	app: {
 		title: 'javascript-fullstack-store - Development Environment'
 	},
